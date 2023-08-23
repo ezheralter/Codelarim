6856c7de4e6b43f692d503697b8b9b360172d25e
ezheralter <ironjawtrend@gmail.com>
2023-08-23 16:13:35 +0300

    Yeni konular

---
+++ array-3/app.js
@@ -0,0 +1,21 @@
+let ulkeler = ["Türkiye", "Nazi", "Sovyetler Birliği", "Japonya", "Çin", "Fransa"];
+
+
+
+// Array Uzunluğu
+console.log(ulkeler.length);
+
+
+
+//Array e Ekleme
+ulkeler.push("Kanada");
+console.log(ulkeler)
+
+
+
+//concat: Array Birleştirme ,Değişkeni Etkilemez 
+let yeniUlkeler = ["Kuveyt", "Mısır"];
+let tumUlkeler = ulkeler.concat(yeniUlkeler);
+console.log(tumUlkeler);
+
+
