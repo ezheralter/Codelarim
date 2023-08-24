dbbaf615d436e741d4ff4c1f201314b1d9f9ab16
ezheralter <ironjawtrend@gmail.com>
2023-08-24 13:09:56 +0300

    odev

---
+++ odev-3/app.js
@@ -0,0 +1,24 @@
+let sehirler = ["Van", "Siirt", "Gaziantep", "Bursa", "Mersin"];
+
+
+A = ("Ankara");
+
+
+sehirler.push("A");
+
+
+
+
+document.write(sehirler[3]);
+
+A = ("Antalya")
+
+
+console.log(sehirler);
+
+
+
+
+
+
+
