981c76d31c524cbe24dffa380e9346b74ff0e3b6
ezheralter <ironjawtrend@gmail.com>
2023-08-26 14:52:51 +0300

    odevler

---
+++ odev-4/app.js
@@ -0,0 +1,7 @@
+let cicekler = ["Papatya", "Boru Çiçeği", "Deve Tabanı", "Karanfil", "Gül"];
+
+
+
+for (let n = 0; n < cicekler.length; n++) {
+    document.write("- " + ".Çiçek- " + cicekler[n] + "<br>");
+}
