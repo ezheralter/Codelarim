d6359df06619db2c7fbcc701f17467e86cb1a0b5
ezheralter <ironjawtrend@gmail.com>
2023-09-20 23:06:15 +0300

    sonunda

---
+++ odev-21/app.js
@@ -0,0 +1,34 @@
+const kediAtalari = [
+    {
+        ad: "Aslan",
+        nufus: 39000,
+        yasadigiYer: "Afrika",
+        turler: [
+            { ad: "Asya Aslanı", nufus: 411 },
+            { ad: "Kuzey Doğu Kongo Aslanı", nufus: "Nesli Tükenmiştir" },
+            { ad: "Güney Afrika Aslanı", nufus: 47.000 }
+        ]
+    },
+    {
+        ad: "Çita",
+        nufus: 7000,
+        yasadigiYer: "Afrika",
+        turler: [
+            { ad: "Kuzey Afrika Çitası", nufus: 82 },
+            { ad: "Asya Çitası", nufus: 7000 },
+            { ad: "Kuzeybatı Çitası", nufus: 84 }
+        ]
+    },
+    {
+        ad: "Leopar",
+        nufus: 1300,
+        yasadigiYer: "Birsürü",
+        turler: [
+            { ad: "Avrupa parsı", nufus: "2.161 milyon" },
+            { ad: "Afrika parsı", nufus: "?" },
+            { ad: "Amur parsı", nufus: 60 }
+        ]
+    }
+];
+
+console.log(kediAtalari);
